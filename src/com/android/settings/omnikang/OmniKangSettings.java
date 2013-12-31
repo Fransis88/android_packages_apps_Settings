eba2ed40f9618613a1ce462a8a7b638d335dfde7
Alexander Martinz <eviscerationls@gmail.com>
2013-12-31 18:20:55 +0100

    navbar: show menu if nav bars are force enabled via property
    users can force enable navigation bars, via setting the
    property "qemu.hw.mainkeys=0".
    after a reboot, they have fully functional navigation bars.
    
    unfortunatly the omnigears setting doesnt recognize them, as it
    doesnt check for it.
    
    lets check for this property and let users, which force enable
    their navigation bar, enjoy the option to customize them.
    
    Change-Id: I2b8b4a74d50f2d13ff15840845e64f7cf42f7d87

--- src/com/android/settings/omnikang/OmniKangSettings.java
+++ src/com/android/settings/omnikang/OmniKangSettings.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.RemoteException;
+import android.os.SystemProperties;
 import android.provider.Settings;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
@@ -65,7 +66,11 @@ public class OmniKangSettings extends SettingsPreferenceFragment implements
         mQSQuickAccess_linked.setChecked((Settings.System.getInt(resolver,
                 Settings.System.QS_QUICK_ACCESS_LINKED, 0) == 1));
 
-        boolean hasNavBar = getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
+        boolean hasNavBar = getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar);
+        // Also check, if users without navigation bar force enabled it.
+        hasNavBar = hasNavBar || (SystemProperties.getInt("qemu.hw.mainkeys", 1) == 0);
+
         // Hide navigation bar category on devices without navigation bar
         if (!hasNavBar) {
             prefSet.removePreference(findPreference(CATEGORY_NAVBAR));
