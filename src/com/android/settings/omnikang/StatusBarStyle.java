3c2c10213add2fa305629baf62a04b8d16bbdb38
Schubi <schubi@erlangen.ccc.de>
2014-01-19 01:58:23 +0100

    [2/2] Settings: System bar & Status icon color
    Change-Id: I1331935bc2f446bb73a287af5c5fab5c2325b0b3

--- src/com/android/settings/omnikang/StatusBarStyle.java
+++ src/com/android/settings/omnikang/StatusBarStyle.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.omnikang;
 
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
@@ -25,19 +26,37 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import com.android.settings.util.Helpers;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 public class StatusBarStyle extends SettingsPreferenceFragment implements
 OnPreferenceChangeListener {
     
+    private static final String TAG = "StatusBarStyle";
+
     private static final String QUICK_PULLDOWN = "quick_pulldown";
     private static final String PRE_COLLAPSE_PANEL = "collapse_panel";
 
+    private static final String PREF_CUSTOM_STATUS_BAR_COLOR = "custom_status_bar_color";
+    private static final String PREF_STATUS_BAR_OPAQUE_COLOR = "status_bar_opaque_color";
+//    private static final String PREF_STATUS_BAR_SEMI_TRANS_COLOR = "status_bar_trans_color";
+    private static final String PREF_CUSTOM_SYSTEM_ICON_COLOR = "custom_system_icon_color";
+    private static final String PREF_SYSTEM_ICON_COLOR = "system_icon_color";
+
     private ListPreference mQuickPulldown;
     CheckBoxPreference mCollapsePanel;
+
+    private CheckBoxPreference mCustomBarColor;
+    private ColorPickerPreference mBarOpaqueColor;
+//    private ColorPickerPreference mBarTransColor;
+    private CheckBoxPreference mCustomIconColor;
+    private ColorPickerPreference mIconColor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -58,6 +77,69 @@ OnPreferenceChangeListener {
         mCollapsePanel.setChecked(Settings.System.getInt(getContentResolver(),
         Settings.System.QS_COLLAPSE_PANEL, 0) == 1);
         mCollapsePanel.setOnPreferenceChangeListener(this);
+
+        int intColor;
+        String hexColor;
+
+
+        PackageManager pm = getPackageManager();
+        Resources systemUiResources;
+        try {
+            systemUiResources = pm.getResourcesForApplication("com.android.systemui");
+        } catch (Exception e) {
+            Log.e(TAG, "can't access systemui resources",e);
+            return;
+        }
+
+        mCustomBarColor = (CheckBoxPreference) findPreference(PREF_CUSTOM_STATUS_BAR_COLOR);
+        mCustomBarColor.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.CUSTOM_STATUS_BAR_COLOR, 0) == 1);
+
+        mCustomIconColor = (CheckBoxPreference) findPreference(PREF_CUSTOM_SYSTEM_ICON_COLOR);
+        mCustomIconColor.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.CUSTOM_SYSTEM_ICON_COLOR, 0) == 1);
+
+        mBarOpaqueColor = (ColorPickerPreference) findPreference(PREF_STATUS_BAR_OPAQUE_COLOR);
+        mBarOpaqueColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_OPAQUE_COLOR, 0xff000000);
+        mBarOpaqueColor.setSummary(getResources().getString(R.string.default_string));
+        if (intColor == 0xff000000) {
+            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
+                    "com.android.systemui:color/system_bar_background_opaque", null, null));
+        } else {
+            hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mBarOpaqueColor.setSummary(hexColor);
+        }
+        mBarOpaqueColor.setNewPreviewColor(intColor);
+
+//        mBarTransColor = (ColorPickerPreference) findPreference(PREF_STATUS_BAR_SEMI_TRANS_COLOR);
+//        mBarTransColor.setOnPreferenceChangeListener(this);
+//        intColor = Settings.System.getInt(getActivity().getContentResolver(),
+//                    Settings.System.STATUS_BAR_SEMI_TRANS_COLOR, 0x66000000);
+//        mBarTransColor.setSummary(getResources().getString(R.string.default_string));
+//        if (intColor == 0xff000000) {
+//            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
+//                    "com.android.systemui:color/system_bar_background_semi_transparent", null, null));
+//        } else {
+//            hexColor = String.format("#%08x", (0x66ffffff & intColor));
+//            mBarTransColor.setSummary(hexColor);
+//        }
+//        mBarTransColor.setNewPreviewColor(intColor);
+
+        mIconColor = (ColorPickerPreference) findPreference(PREF_SYSTEM_ICON_COLOR);
+        mIconColor.setOnPreferenceChangeListener(this);
+        intColor = Settings.System.getInt(getContentResolver(),
+                    Settings.System.SYSTEM_ICON_COLOR, -1);
+        mIconColor.setSummary(getResources().getString(R.string.default_string));
+        if (intColor == 0xffffffff) {
+            intColor = systemUiResources.getColor(systemUiResources.getIdentifier(
+                    "com.android.systemui:color/status_bar_clock_color", null, null));
+        } else {
+           hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mIconColor.setSummary(hexColor);
+        }
+        mIconColor.setNewPreviewColor(intColor);
     }
 
     private void updatePulldownSummary(int value) {
@@ -82,12 +164,53 @@ OnPreferenceChangeListener {
         } else if (preference == mCollapsePanel) {
             Settings.System.putInt(getContentResolver(), Settings.System.QS_COLLAPSE_PANEL, (Boolean) objValue ? 1 : 0);
             return true;
+        } else if (preference == mBarOpaqueColor) {
+            String hex = ColorPickerPreference.convertToARGB(Integer
+                    .valueOf(String.valueOf(objValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.STATUS_BAR_OPAQUE_COLOR, intHex);
+            Helpers.restartSystemUI();
+            return true;
+        } else if (preference == mIconColor) {
+            String hex = ColorPickerPreference.convertToARGB(Integer
+                    .valueOf(String.valueOf(objValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SYSTEM_ICON_COLOR, intHex);
+            Helpers.restartSystemUI();
+            return true;
+//        } else if (preference == mBarTransColor) {
+//            String hex = ColorPickerPreference.convertToARGB(Integer
+//                    .valueOf(String.valueOf(objValue)));
+//            preference.setSummary(hex);
+//            int intHex = ColorPickerPreference.convertToColorInt(hex);
+//            Settings.System.putInt(getActivity().getContentResolver(),
+//                    Settings.System.STATUS_BAR_SEMI_TRANS_COLOR, intHex);
+//            Helpers.restartSystemUI();
+//            return true;
         }
         return false;
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
-        return false;
+
+        if (preference == mCustomBarColor) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.CUSTOM_STATUS_BAR_COLOR,
+            mCustomBarColor.isChecked() ? 1 : 0);
+            Helpers.restartSystemUI();
+            return true;
+        } else if (preference == mCustomIconColor) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.CUSTOM_SYSTEM_ICON_COLOR,
+            mCustomIconColor.isChecked() ? 1 : 0);
+            Helpers.restartSystemUI();
+            return true;
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 }
