afa2d41011a1e123b647abe027eba88cb5a74303
Schubi <schubi@erlangen.ccc.de>
2014-04-24 12:41:53 +0200

    Revert "Fix missing string"
    This reverts commit 69b7fc5b959fc124cc92cf009a5da9e206699357.

--- src/com/android/settings/omnikang/LockscreenTargets.java
+++ src/com/android/settings/omnikang/LockscreenTargets.java
@@ -134,7 +134,7 @@ public class LockscreenTargets extends Fragment implements
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.add(0, MENU_RESET, 0, R.string.reset)
+        menu.add(0, MENU_RESET, 0, R.string.profile_reset_title)
             .setIcon(R.drawable.ic_settings_backup) // use the backup icon
             .setAlphabeticShortcut('r')
             .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM |
